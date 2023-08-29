6bfbdecfa6584fcf173aaf662d6341a833cca216
HyerinJeon <hyerin0930@gmail.com>
2023-08-29 10:06:34 +0900

    공지사항 삭제 API 추가

--- src/pages/notice/NoticeDetail.tsx
+++ src/pages/notice/NoticeDetail.tsx
@@ -4,6 +4,7 @@ import * as S from "../../styles/notice/NoticeDetail.styles";
 import { useState, useEffect } from "react";
 import { Viewer } from "@toast-ui/react-editor";
 import { INoticeDetailResponse } from "../../types/notice/noticeDetail.types";
+import { getNoticeDelete } from "../../api/notice/getNoticeDelete";
 
 export default function NoticeDetail() {
   const [contents, setContents] = useState<INoticeDetailResponse>();
@@ -15,10 +16,15 @@ export default function NoticeDetail() {
     Number(params.noticeId),
     setContents,
   );
+  const { mutate: noticeDeleteMutate } = getNoticeDelete();
 
   useEffect(() => {
     noticeDetailMutate();
   }, []);
+
+  const onClickDeleteNotice = () => {
+    noticeDeleteMutate(params.noticeId);
+  };
 
   const onClickMoveToList = () => {
     navigate("/notice/list");
@@ -30,12 +36,14 @@ export default function NoticeDetail() {
         <S.Title>제목</S.Title>
         <S.TopContainer>
           <S.InfoContainer>
-            <S.Info>2023-08-28 09:00:00</S.Info>
             <S.Info>작성자: 작성자</S.Info>
+            <S.Info>2023-08-28 09:00:00</S.Info>
           </S.InfoContainer>
           <S.ButtonContainer>
             <S.EditDeleteButton>수정</S.EditDeleteButton>
-            <S.EditDeleteButton>삭제</S.EditDeleteButton>
+            <S.EditDeleteButton onClick={onClickDeleteNotice}>
+              삭제
+            </S.EditDeleteButton>
             <S.EditDeleteButton onClick={onClickMoveToList}>
               목록
             </S.EditDeleteButton>
