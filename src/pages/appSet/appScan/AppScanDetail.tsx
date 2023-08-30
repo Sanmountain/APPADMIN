d8d9b3fd4f78a299c0879ebee95cecde9ae35a80
HyerinJeon <hyerin0930@gmail.com>
2023-08-30 11:22:41 +0900

    스캔사용자 detail page CSS 및 data 붙이기

--- src/pages/appSet/appScan/AppScanDetail.tsx
+++ src/pages/appSet/appScan/AppScanDetail.tsx
@@ -1,3 +1,36 @@
+import { useRecoilValue } from "recoil";
+import * as S from "../../../styles/appSet/AppScanDetail.styles";
+import { scanUserListState } from "../../../stores/scanUserListState";
+import { useParams } from "react-router";
+
 export default function AppScanDetail() {
-  return <></>;
+  const scanUserList = useRecoilValue(scanUserListState);
+
+  const params = useParams<{ scanDate?: string }>();
+  const scanDate = params.scanDate;
+
+  const scanDetailList = scanUserList?.[scanDate || ""] || [];
+
+  return (
+    <S.Container>
+      <S.TopContainer>
+        <S.BackIcon />
+        <S.TextContainer>날짜 : {scanDate}</S.TextContainer>
+        <S.TextContainer>사용자수 : {scanDetailList.length} 명</S.TextContainer>
+      </S.TopContainer>
+
+      <S.TitleContainer>
+        <S.Title>사원코드</S.Title>
+        <S.Title>전화번호</S.Title>
+      </S.TitleContainer>
+      <S.ContentsListContainer>
+        {scanDetailList.map((item, index: number) => (
+          <S.ContentsContainer key={index}>
+            <S.Contents>{item.tradesub_cd}</S.Contents>
+            <S.Contents>{item.tradesub_tel}</S.Contents>
+          </S.ContentsContainer>
+        ))}
+      </S.ContentsListContainer>
+    </S.Container>
+  );
 }
