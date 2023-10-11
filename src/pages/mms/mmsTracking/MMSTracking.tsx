911d79a6ffb9538b07ffc1aba658a77a39ab5565
HyerinJeon <hyerin0930@gmail.com>
2023-10-11 16:15:53 +0900

    MMS추적 페이지 CSS 수정 및 API 추가

--- src/pages/mms/mmsTracking/MMSTracking.tsx
+++ src/pages/mms/mmsTracking/MMSTracking.tsx
@@ -1,42 +1,156 @@
-import { useNavigate } from "react-router";
 import CommonButton from "../../../components/common/CommonButton";
 import * as S from "../../../styles/mms/MMSTracking.styles";
+import { ChangeEvent, useState } from "react";
+import { getTracking } from "../../../api/mms/getTracking";
+import {
+  IBarcodeInfoList,
+  ITrackingDetail,
+  ITrackingLogList,
+} from "../../../types/mms/tracking.types";
+import Swal from "sweetalert2";
 
 export default function MMSTracking() {
-  const navigate = useNavigate();
+  const [trackingDetail, setTrackingDetail] = useState<ITrackingDetail>();
+  const [trackingInfoList, setTrackingInfoList] = useState<IBarcodeInfoList[]>(
+    [],
+  );
+  const [trackingLogList, setTrackingLogList] = useState<ITrackingLogList[]>(
+    [],
+  );
+  const [deliveryStatus, setDeliveryStatus] = useState("");
+  const [barcode, setBarcode] = useState("");
 
-  const onClickMoveToDetail = (id: string) => {
-    navigate(`/mms/tracking/detail/${id}`);
+  const { mutate: trackingListMutate } = getTracking(
+    barcode,
+    setTrackingDetail,
+    setTrackingInfoList,
+    setTrackingLogList,
+    setDeliveryStatus,
+  );
+
+  const handleBarcode = (e: ChangeEvent<HTMLInputElement>) => {
+    setBarcode(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (!barcode) {
+      Swal.fire({
+        icon: "warning",
+        title: "송장번호를 입력해주세요.",
+        confirmButtonText: "확인",
+      });
+    } else trackingListMutate();
   };
 
   return (
     <S.Container>
       <S.TopContainer>
         <S.FilterContainer>
-          <S.Input placeholder="송장번호" />
-          <CommonButton contents="조회" onClickFn={() => console.log("dddd")} />
+          <S.FilterTitle>운송장번호</S.FilterTitle>
+          <S.Input placeholder="송장번호" onChange={handleBarcode} />
+          <CommonButton contents="조회" onClickFn={handleSearch} />
         </S.FilterContainer>
       </S.TopContainer>
 
-      <S.TitleContainer>
-        <S.Title>배송기사 전화</S.Title>
-        <S.Title>상태</S.Title>
-        <S.Title>배송</S.Title>
-        <S.Title>내역</S.Title>
-      </S.TitleContainer>
-      <S.ContentsListContainer>
-        <S.ContentsContainer>
-          <S.Contents>010-1111-1111</S.Contents>
-          <S.Contents>배송출발</S.Contents>
-          <S.Contents>새벽배송</S.Contents>
-          <S.Contents>
-            <CommonButton
-              contents="상세"
-              onClickFn={() => onClickMoveToDetail("1")}
-            />
-          </S.Contents>
-        </S.ContentsContainer>
-      </S.ContentsListContainer>
+      {trackingDetail && (
+        <>
+          <S.TitleContainer>
+            <S.Title>운송장번호</S.Title>
+            <S.Title>보내는 분</S.Title>
+            <S.Title>받는 분</S.Title>
+            <S.Title>상품 정보</S.Title>
+          </S.TitleContainer>
+          <S.ContentsListContainer>
+            <S.ContentsContainer>
+              <S.Contents>{trackingDetail?.iv_no}</S.Contents>
+              <S.Contents>{trackingDetail?.pk_nm}</S.Contents>
+              <S.Contents>{trackingDetail?.dv_nm}</S.Contents>
+              <S.Contents>{trackingDetail?.th_nm}</S.Contents>
+            </S.ContentsContainer>
+          </S.ContentsListContainer>
+
+          <S.ShipmentContainer>
+            <S.IconTitleContainer>
+              <S.IconContainer>
+                <S.ComputerIcon
+                  className={deliveryStatus === "집하" ? "active" : ""}
+                />
+              </S.IconContainer>
+              <S.IconTitle>집하</S.IconTitle>
+            </S.IconTitleContainer>
+            <S.IconTitleContainer>
+              <S.IconContainer>
+                <S.WarehouseIcon
+                  className={deliveryStatus === "입고" ? "active" : ""}
+                />
+              </S.IconContainer>
+              <S.IconTitle>입고</S.IconTitle>
+            </S.IconTitleContainer>
+            <S.IconTitleContainer>
+              <S.IconContainer>
+                <S.HouseIcon
+                  className={deliveryStatus === "도착" ? "active" : ""}
+                />
+              </S.IconContainer>
+              <S.IconTitle>도착</S.IconTitle>
+            </S.IconTitleContainer>
+            <S.IconTitleContainer>
+              <S.IconContainer>
+                <S.TruckIcon
+                  className={deliveryStatus === "출발" ? "active" : ""}
+                />
+              </S.IconContainer>
+              <S.IconTitle>출발</S.IconTitle>
+            </S.IconTitleContainer>
+            <S.IconTitleContainer>
+              <S.IconContainer>
+                <S.HouseCheckIcon
+                  className={deliveryStatus === "완료" ? "active" : ""}
+                />
+              </S.IconContainer>
+              <S.IconTitle>완료</S.IconTitle>
+            </S.IconTitleContainer>
+          </S.ShipmentContainer>
+
+          <S.TableContainer>
+            <S.TableTitle>화물 추적</S.TableTitle>
+            <S.TitleContainer className="tracking">
+              <S.Title>단계</S.Title>
+              <S.Title>시간</S.Title>
+              <S.Title>현재위치</S.Title>
+            </S.TitleContainer>
+            <S.ContentsListContainer>
+              {trackingInfoList?.map((item, index) => (
+                <S.ContentsContainer key={index} className="tracking">
+                  <S.Contents>{item.point_nm}</S.Contents>
+                  <S.Contents>{item.scan_ymd}</S.Contents>
+                  <S.Contents>{item.trade_nm}</S.Contents>
+                </S.ContentsContainer>
+              ))}
+            </S.ContentsListContainer>
+          </S.TableContainer>
+
+          <S.TableContainer>
+            <S.TableTitle>로그 내역</S.TableTitle>
+            <S.TitleContainer className="log">
+              <S.Title>스캔코드</S.Title>
+              <S.Title>시간</S.Title>
+              <S.Title>상태</S.Title>
+              <S.Title>화면 이름</S.Title>
+            </S.TitleContainer>
+            <S.ContentsListContainer>
+              {trackingLogList?.map((item, index) => (
+                <S.ContentsContainer key={index} className="log">
+                  <S.Contents>{item.scan_cd}</S.Contents>
+                  <S.Contents>{item.log_dt}</S.Contents>
+                  <S.Contents>{item.log_state}</S.Contents>
+                  <S.Contents>{item.screen_nm}</S.Contents>
+                </S.ContentsContainer>
+              ))}
+            </S.ContentsListContainer>
+          </S.TableContainer>
+        </>
+      )}
     </S.Container>
   );
 }
--- src/styles/mms/MMSTracking.styles.ts
+++ src/styles/mms/MMSTracking.styles.ts
@@ -1,33 +1,48 @@
 import styled from "styled-components";
+import {
+  FaComputer,
+  FaWarehouse,
+  FaTruckFast,
+  FaHouseChimney,
+  FaHouseCircleCheck,
+} from "react-icons/fa6";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-
+  align-items: center;
   width: 100%;
 `;
 
 export const TopContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
 
   width: 100%;
-  height: 50px;
+  height: 100px;
   padding: 0 10px;
+  background-color: #f5f5f5;
 `;
 
 export const FilterContainer = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
 
   width: 100%;
-  height: 35px;
-  gap: 15px;
+  height: 40px;
+  gap: 30px;
   padding: 0 10px;
 `;
 
+export const FilterTitle = styled.label`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
 export const Input = styled.input`
-  width: 200px;
+  width: 350px;
   height: 100%;
 `;
 
@@ -39,7 +54,15 @@ export const TitleContainer = styled.div`
   background-color: #f8f9fa;
   width: 100%;
   height: 50px;
-  margin-top: 10px;
+  margin-top: 30px;
+
+  &.tracking {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  &.log {
+    grid-template-columns: 0.5fr 1fr 2fr 1fr;
+  }
 `;
 
 export const Title = styled.div`
@@ -79,14 +102,126 @@ export const ContentsContainer = styled.div`
   border-radius: 5px;
   width: 100%;
   height: 40px;
+
+  &.tracking {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  &.log {
+    grid-template-columns: 0.5fr 1fr 2fr 1fr;
+  }
 `;
 
 export const Contents = styled.div`
   font-size: 16px;
 `;
 
-export const PaginationContainer = styled.div`
-  width: 100%;
+export const ShipmentContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 85%;
+  height: 150px;
+  margin-top: 50px;
+  margin-bottom: 70px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 43%;
+    left: 0.5%;
+    right: 0;
+    width: 99%;
+    height: 2px;
+    background-color: #bdbdbd;
+  }
+`;
+
+export const IconTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100px;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  background-color: #f8f9fa;
+  border-radius: 50%;
+  z-index: 2;
+`;
+
+export const IconTitle = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: 10px;
+`;
+
+export const ComputerIcon = styled(FaComputer)`
+  width: 50px;
   height: 50px;
-  margin-top: 20px;
+  color: black;
+
+  &.active {
+    color: #ff4b2b;
+  }
+`;
+
+export const WarehouseIcon = styled(FaWarehouse)`
+  width: 50px;
+  height: 50px;
+  color: black;
+
+  &.active {
+    color: #ff4b2b;
+  }
+`;
+
+export const TruckIcon = styled(FaTruckFast)`
+  width: 50px;
+  height: 50px;
+  color: black;
+
+  &.active {
+    color: #ff4b2b;
+  }
+`;
+
+export const HouseIcon = styled(FaHouseChimney)`
+  width: 50px;
+  height: 50px;
+  color: black;
+
+  &.active {
+    color: #ff4b2b;
+  }
+`;
+
+export const HouseCheckIcon = styled(FaHouseCircleCheck)`
+  width: 50px;
+  height: 50px;
+  color: black;
+
+  &.active {
+    color: #ff4b2b;
+  }
+`;
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
+
+export const TableTitle = styled.div`
+  font-size: 20px;
+  font-weight: 700;
 `;
