08d74a4479bc0043d467b716a6964437a7a1c38a
HyerinJeon <hyerin0930@gmail.com>
2023-11-15 10:22:25 +0900

    기존 핸드폰번호 신규등록 시 수정으로 처리되도록 수정

--- src/API/appSet/appPaymentUser/getPaymentUserRegist.tsx
+++ src/API/appSet/appPaymentUser/getPaymentUserRegist.tsx
@@ -55,6 +55,25 @@ export const getPaymentUserRegist = (
             title: "등록되었습니다",
             confirmButtonText: "확인",
           });
+        }
+        // NOTE 기존에 등록돼 있는 핸드폰번호 신규등록 시 수정으로 처리
+        else if (data.result === "77") {
+          paymentUserListMutate();
+
+          setPaymentUser({
+            user_id: "",
+            phone_no: "",
+            payment_date: dayjs().format("YYYY-MM-DD"),
+            expire_date: dayjs().format("YYYY-MM-DD"),
+            free_user: "N",
+            qa: 0,
+          });
+
+          Swal.fire({
+            icon: "success",
+            title: "수정되었습니다",
+            confirmButtonText: "확인",
+          });
         } else if (data.result === "04") {
           Swal.fire({
             title: "Error!",
